4cf2e9fe593cf2eebcd3eb9543c7777afe434fc2
Matt Eland <Matt.Eland@GMail.com>
2019-07-28 22:57:10 -0400

    Added a VMBase class.

---
+++ MattEland.Shared.WPF/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace MattEland.Shared.WPF
+{
+    /// <summary>
+    /// A common class for notifying of property changed events
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies when a specific property changes
+        /// </summary>
+        /// <param name="propertyName"></param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName, CanBeNull] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Notifies that all properties on the object have changed
+        /// </summary>
+        protected virtual void OnAllPropertiesChanged() => OnPropertyChanged(string.Empty);
+    }
+}
